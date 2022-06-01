32c7a72632d02c2ee7ccba7e925bea11d0553dd3
akshay-kumar7 <47611108+akshay-kumar7@users.noreply.github.com>
2022-06-01 17:36:52 -0400

    Updated EmployeeCrud.java with login validation

--- reimbursement-expense-system/src/main/java/EmployeeCrud.java
+++ reimbursement-expense-system/src/main/java/EmployeeCrud.java
@@ -14,19 +14,23 @@ public class EmployeeCrud {
 			Javalin server = Javalin.create((config) -> config.enableCorsForAllOrigins());
 			server.start(7474);
 			
-			
+			//GET ALL EMPLOYEES
 			server.get("/employees", (ctx)->{
 			// here we contact service, service contacts dao 
-			// dao fetches all the books and return it back here
-			
 			// allEmployees contains all the employees fetched from the DB
 			List<EmployeePojo> allEmployees = employeeService.getAllEmployees();
 			
 			//now put the books in the response body, it has to converted to json format, 
 			// the ctx.json() will take care of the above 2 and sends back the response to the client/consumer
 			ctx.json(allEmployees);
-			
-		});
+			});
+		
+		
+			// endpoint for login validation
+			server.post("/employees/login", (ctx) -> {
+				EmployeePojo loginEmpPojo = ctx.bodyAsClass(EmployeePojo.class);
+				ctx.json(employeeService.login(loginEmpPojo));
+			});
 
 
 	}
