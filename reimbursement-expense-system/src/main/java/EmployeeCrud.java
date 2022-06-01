8fdc1a35fa85fabef90799d6c5a76d726db509b3
Isaac Suttle <isaacsuttle@gmail.com>
2022-06-01 01:09:28 -0400

    Get all employees almost done

--- reimbursement-expense-system/src/main/java/EmployeeCrud.java
+++ reimbursement-expense-system/src/main/java/EmployeeCrud.java
@@ -1,18 +1,30 @@
+import java.util.List;
+
+import io.javalin.Javalin;
+import model.EmployeePojo;
+import service.EmployeeService;
+import service.EmployeeServiceImpl;
 
 public class EmployeeCrud {
 
 	public static void main(String[] args) {
 		
-		server.get("/employees", (ctx)->{
+			EmployeeService employeeService = new EmployeeServiceImpl();
+			
+			Javalin server = Javalin.create();
+			server.start(7474);
+			
+			
+			server.get("/employees", (ctx)->{
 			// here we contact service, service contacts dao 
 			// dao fetches all the books and return it back here
 			
 			// allEmployees contains all the employees fetched from the DB
-			List<BookPojo> allBooks = bookService.getAllBooks();
+			List<EmployeePojo> allEmployees = employeeService.getAllEmployees();
 			
 			//now put the books in the response body, it has to converted to json format, 
 			// the ctx.json() will take care of the above 2 and sends back the response to the client/consumer
-			ctx.json(allBooks);
+			ctx.json(allEmployees);
 			
 		});
 
