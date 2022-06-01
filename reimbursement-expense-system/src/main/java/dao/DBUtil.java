0e562649011dbcaf2c998c8e617ad0376071ecb1
akshay-kumar7 <47611108+akshay-kumar7@users.noreply.github.com>
2022-06-01 17:31:54 -0400

    Update DBUtil.java
    Updated Conn

--- reimbursement-expense-system/src/main/java/dao/DBUtil.java
+++ reimbursement-expense-system/src/main/java/dao/DBUtil.java
@@ -18,7 +18,7 @@ public class DBUtil {
 
 	public static Connection makeConnection()throws SQLException {
 		if (conn == null) {
-			conn = DriverManager.getConnection("jdbc:postgresql://expense-reimbursement-system.cvjcx3y0oyxm.ca-central-1.rds.amazonaws.com/ers", "postgres", "postgres");
+			conn = DriverManager.getConnection("jdbc:postgresql://expense-reimbursement-system.cvjcx3y0oyxm.ca-central-1.rds.amazonaws.com:5432/ers", "postgres", "postgres");
 		}
 		return conn;
 	}
