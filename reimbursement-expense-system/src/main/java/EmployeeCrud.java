15d6d51268b5e118f9f6a80110da512bc3490aab
akshay-kumar7 <47611108+akshay-kumar7@users.noreply.github.com>
2022-06-03 10:45:33 -0400

    Re-upload Combine

--- reimbursement-expense-system/src/main/java/EmployeeCrud.java
+++ reimbursement-expense-system/src/main/java/EmployeeCrud.java
@@ -11,67 +11,49 @@ import service.ReimbursementServiceImpl;
 public class EmployeeCrud {
 
 	public static void main(String[] args) {
-		
+
 		EmployeeService employeeService = new EmployeeServiceImpl();
 		ReimbursementService reimbursementService = new ReimbursementServiceImpl();
-		
+
 		Javalin server = Javalin.create((config) -> config.enableCorsForAllOrigins());
 		server.start(7474);
-		
-		//GET ALL EMPLOYEES
-		server.get("/employees", (ctx)->{
-			// here we contact service, service contacts dao 
+
+		// GET ALL EMPLOYEES
+		server.get("/employees", (ctx) -> {
+			// here we contact service, service contacts dao
 			// allEmployees contains all the employees fetched from the DB
 			List<EmployeePojo> allEmployees = employeeService.getAllEmployees();
-			
-			//now put the books in the response body, it has to converted to json format, 
-			// the ctx.json() will take care of the above 2 and sends back the response to the client/consumer
+
+			// now put the books in the response body, it has to converted to json format,
+			// the ctx.json() will take care of the above 2 and sends back the response to
+			// the client/consumer
 			ctx.json(allEmployees);
 		});
-	
-	
+
 		// endpoint for login validation
 		server.post("/employees/login", (ctx) -> {
 			EmployeePojo loginEmpPojo = ctx.bodyAsClass(EmployeePojo.class);
 			ctx.json(employeeService.login(loginEmpPojo));
 		});
-		
-		// get all reimbursement
-		server.get("/reimbursement", (ctx)->{
+
+
+
+		// **************************************************REIMBURSEMENT CRUD OPERATION***************************************************
+
+		// Get All Reimbursement Request
+		server.get("/reimbursement", (ctx) -> {
 			List<ReimbursementPojo> allRequests = reimbursementService.viewAllRequests();
 			ctx.json(allRequests);
 		});
-		
+
 		server.post("/reimbursement", (ctx) -> {
-			
+
 			ReimbursementPojo newReimbursementPojo = ctx.bodyAsClass(ReimbursementPojo.class);
-			
-			
+
 			ReimbursementPojo returnBookPojo = reimbursementService.submitRequest(newReimbursementPojo);
-			
-			
+
 			ctx.json(returnBookPojo);
 		});
-		
-		
-		
-		//**************************************************REIMBURSEMENT CRUD OPERATION***************************************************
-		
-		//Get All Reimbursement Request
-		server.get("/reimbursement", (ctx)->{
-			List<ReimbursementPojo> allRequests = reimbursementService.viewAllRequests();
-			ctx.json(allRequests);
-		});
-		server.post("/reimbursement", (ctx) -> {
-			
-			ReimbursementPojo newReimbursementPojo = ctx.bodyAsClass(ReimbursementPojo.class);
-			
-			ReimbursementPojo returnBookPojo = reimbursementService.submitRequest(newReimbursementPojo);
-			
-			ctx.json(returnBookPojo);
-		});
-		
-		
 
 	}
 
